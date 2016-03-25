b84eeeaac8aa2560e0fdc32cd7f5f24ed662b78c
ybrs <aybars.badur@gmail.com>
2016-03-25 17:44:17 +0200

    added info, fixed fabric rest of the commandline

--- assh/assh.py
+++ assh/assh.py
@@ -1,10 +1,12 @@
+#-*- coding: utf-8 -*-
+
 import boto.ec2
 
 import imp
+import subprocess
 
-
-def get_instances(aws_key, aws_secret,**tags):
-    conn = boto.ec2.connect_to_region('us-east-1', # n virginia
+def get_instances(region, aws_key, aws_secret, **tags):
+    conn = boto.ec2.connect_to_region(region, # n virginia
             aws_access_key_id=aws_key,
             aws_secret_access_key=aws_secret)
 
@@ -39,6 +41,7 @@ import sys
 import argparse
 import thread
 import time
+from hst.hst import Picker, QuitException
 
 import locale
 locale.setlocale(locale.LC_ALL,"")
@@ -47,16 +50,19 @@ logger = logging.getLogger(__name__)
 
 
 class SimpleLineLoader(object):
-    def __init__(self, aws_key, aws_secret):
+    def __init__(self, aws_region, aws_key, aws_secret):
         self.aws_key = aws_key
         self.aws_secret = aws_secret
+        self.aws_region = aws_region
 
     def load(self):
-        self.instances = get_instances(self.aws_key, self.aws_secret)
+        self.instances = get_instances(self.aws_region, self.aws_key, self.aws_secret)
         lines = []
         for i in self.instances:
             line = []
             line.append(i.public_dns_name.ljust(50))
+            line.append(' | ')
+            line.append(i.id.ljust(10))
             line.append(' | ')
             for k, v in i.tags.items():
                 line.append('%s = %s' % (k, v))
@@ -64,14 +70,13 @@ class SimpleLineLoader(object):
         return lines
 
 
-from hst.hst import Picker, QuitException
-
-
-
 class AsshPicker(Picker):
 
     def get_hostname_from_line(self, line):
         return line.split('|')[0].strip()
+
+    def get_instance_id_from_line(self, line):
+        return line.split('|')[1].strip()
 
     def key_ENTER(self):
         line = self.pick_line()
@@ -108,11 +113,101 @@ class AsshPicker(Picker):
         return 'ssh %s' % line
 
     def cmd_FAB(self, line):
-        return 'fab -H %s %s' % (line, ' '.join(self.args.rest))
+        rest = subprocess.list2cmdline(self.args.rest)
+        return 'fab -H %s %s' % (line, rest)
 
-def get_rest_of_args(rest):
-    # for r in rest:
-    pass
+    def get_instance_by_public_ip(self, public_ip):
+        for i in self.loader.instances:
+            if i.public_dns_name == public_ip:
+                return i
+
+    def cmd_GRAPH_CPU(self, line):
+        instance = self.get_instance_by_public_ip(line)
+        import boto.ec2.cloudwatch
+        import datetime
+        cw = boto.ec2.cloudwatch.connect_to_region(self.loader.aws_region,
+                                                   aws_access_key_id=self.loader.aws_key,
+                                                   aws_secret_access_key=self.loader.aws_secret)
+        stats = cw.get_metric_statistics(
+            300,
+            datetime.datetime.utcnow() - datetime.timedelta(seconds=3200),
+            datetime.datetime.utcnow(),
+            'CPUUtilization',
+            'AWS/EC2',
+            'Average',
+            dimensions={'InstanceId':[instance.id]}
+        )
+
+        stats = sorted(stats, key=lambda x: x['Timestamp'])
+
+        import plotly
+        from plotly.graph_objs import Scatter, Layout
+        plotly.offline.plot({
+        "data": [
+            Scatter(x=[i['Timestamp'] for k, i in enumerate(stats)], y=[i['Average'] for i in stats])
+        ],
+        "layout": Layout(
+                title="CPU - %s" % instance.id,
+                yaxis=dict(range=[0, 100])
+            )
+        })
+
+        ret = []
+        for l in stats:
+            ret.append(l)
+
+        return "%s\n" % ret
+
+    def cmd_INFO(self, line):
+        instance_info = """
+id
+groups - A list of Group objects representing the security groups associated with the instance.
+public_dns_name - The public dns name of the instance.
+private_dns_name - The private dns name of the instance.
+state - The string representation of the instance’s current state.
+state_code - An integer representation of the instance’s current state.
+previous_state - The string representation of the instance’s previous state.
+previous_state_code - An integer representation of the instance’s current state.
+key_name - The name of the SSH key associated with the instance.
+instance_type - The type of instance (e.g. m1.small).
+launch_time - The time the instance was launched.
+image_id - The ID of the AMI used to launch this instance.
+placement - The availability zone in which the instance is running.
+placement_group - The name of the placement group the instance is in (for cluster compute instances).
+placement_tenancy - The tenancy of the instance, if the instance is running within a VPC. An instance with a tenancy of dedicated runs on a single-tenant hardware.
+kernel - The kernel associated with the instance.
+ramdisk - The ramdisk associated with the instance.
+architecture - The architecture of the image (i386|x86_64).
+hypervisor - The hypervisor used.
+virtualization_type - The type of virtualization used.
+product_codes - A list of product codes associated with this instance.
+ami_launch_index - This instances position within it’s launch group.
+monitored - A boolean indicating whether monitoring is enabled or not.
+monitoring_state - A string value that contains the actual value of the monitoring element returned by EC2.
+spot_instance_request_id - The ID of the spot instance request if this is a spot instance.
+subnet_id - The VPC Subnet ID, if running in VPC.
+vpc_id - The VPC ID, if running in VPC.
+private_ip_address - The private IP address of the instance.
+ip_address - The public IP address of the instance.
+platform - Platform of the instance (e.g. Windows)
+root_device_name - The name of the root device.
+root_device_type - The root device type (ebs|instance-store).
+block_device_mapping - The Block Device Mapping for the instance.
+state_reason - The reason for the most recent state transition.
+interfaces - List of Elastic Network Interfaces associated with this instance.
+ebs_optimized - Whether instance is using optimized EBS volumes or not.
+instance_profile - A Python dict containing the instance profile id and arn associated with this instance.
+        """
+
+        ret = []
+        our_instance = self.get_instance_by_public_ip(line)
+        for ln in instance_info.split("\n"):
+            if ln:
+                kv = ln.split('-')
+                k = kv[0].strip()
+                if k:
+                    ret.append(u"%s: %s" % (k.decode('utf8'), getattr(our_instance, k, '')))
+        return "%s\n" % '\n'.join(ret)
 
 def assh():
     parser = argparse.ArgumentParser()
@@ -155,6 +250,7 @@ def assh():
 
     args = parser.parse_args()
 
+
     if args.debug:
         logger.setLevel(logging.DEBUG)
         hdlr = logging.FileHandler(args.logfile)
@@ -168,7 +264,9 @@ def assh():
 
     main(args,
          picker_cls=AsshPicker,
-         loader=SimpleLineLoader(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY))
+         loader=SimpleLineLoader(
+             settings.AWS_REGION,
+             settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY))
 
 if __name__ == '__main__':
     assh()
